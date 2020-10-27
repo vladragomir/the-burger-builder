facc533053d2ab5ec6be31a99888cf02c79ec367
Vlad Dragomir <vdragomir@deloittece.com>
2020-10-27 13:33:42 +0200

    Adding improvements

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,10 +5,6 @@ import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
   // This could be a functional component, doesn't have to be a class
-  componentDidUpdate() {
-    console.log('[OrdersSummary] DidUpdate');
-  }
-
   render() {
     const ingredientSummary = Object.keys(this.props.ingredients).map(
       (igKey) => {
